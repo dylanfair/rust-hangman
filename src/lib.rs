534d51a463b4217325289353183b8ec0549ea189
Dylan <carringtonfair.dylan@gmail.com>
2023-05-12 00:50:57 -0400

    finished the game! just need to clean it up some more now and I'd like to be able to let the user adjust the difficulty

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+/// Makes an API call to get a random word
+pub async fn make_word_request(word_length: usize) -> Result<String, reqwest::Error> {
+    let random_word_url = format!("https://random-word-api.herokuapp.com/word?length={word_length}");
+    let res = reqwest::get(random_word_url).await?;
 
-use reqwest::{Response, Error};
-
-
-pub async fn make_word_request(request_url: &str) -> Result<Response, Error> {
-    let response = reqwest::get(request_url).await?;
-    response
+    let body = res.text().await?;
+    
+    let word = String::from(body).replace(&['[', ']', '"'][..], "");
+    Ok(word)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,77 @@
-fn main() {
-    let RANDOM_WORD_URL = "https://random-word-api.vercel.app/api?words=1";
-    
-    let response = rust_hangman::make_word_request(RANDOM_WORD_URL);
+// #![deny(warnings)]
 
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+
+    const WORD_LENGTH: usize = 5;
+    let mut tries = 7;
+
+    let word = rust_hangman::make_word_request(WORD_LENGTH).await?;
+    
+    // so now we have our word
+    // next would be making x spaces depending on length of word
+    // Array shenanigans
+    const EMPTY_STRING: String = String::new();
+    let mut display_array = [EMPTY_STRING; WORD_LENGTH]; 
+    for i in 0..WORD_LENGTH {
+        display_array[i] = String::from("_");
+    }
+
+    let mut incorrect_guesses = vec![]; // place the incorrect letters a user guesses can go
+    
+    let mut print_statement = "Welcome to hangman!";
+    // loop here
+    loop {
+        clearscreen::clear().expect("failed to clear the screen");
+        println!("{}", display_array.join(" "));
+        println!("{}", print_statement);
+        println!("Incorrect guesses left: {tries}");
+        println!("Word is: {}", word);
+        println!("Here are your incorrect guesses: {:?}", incorrect_guesses);
+        println!("Make your guess: ");
+        let mut guess = String::new();
+        std::io::stdin().read_line(&mut guess).unwrap();
+
+        // Clean guess
+        let guess = guess.replace("\n", "");        
+        if guess.len() > 1 || guess.len() == 0 || !guess.chars().all(char::is_alphabetic) {
+            print_statement = "Your guess must be a single letter.";
+            continue;
+        }
+
+        // See if we guessed that letter already
+        if display_array.contains(&guess) || incorrect_guesses.contains(&guess) {
+            print_statement = "You already guessed that letter, try again.";
+            continue;
+        }
+
+        // Logic if user guesses correctly!
+        if word.contains(&guess) {
+            for (i, char) in word.chars().enumerate() {
+                if &char.to_string() == &guess {
+                    display_array[i] = guess.clone();
+                }
+            }
+            print_statement = "You got it!";
+            
+            if !display_array.contains(&String::from("_")) {
+                clearscreen::clear().expect("failed to clear the screen");
+                println!("{}", display_array.join(" "));
+                println!("Congratulations!! You won the game!");
+                break;
+            }
+            continue;
+        }
+
+        incorrect_guesses.push(guess);
+        print_statement = "Incorrect :(";
+        // Lose condition
+        tries = tries - 1;
+        if tries == 0  {
+            println!("You lost! Feel free to try again!");
+            break;
+        }
+    }
+
+    Ok(())
 }
